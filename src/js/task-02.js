1cd74a6640599664ec14f996885f583b9a3bc3d0
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-05-28 22:31:52 +0300

    добавляет дз

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const array = [];
+ingredients.forEach(function (ingredient) {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  array.push(listItem);
+});
+const list = document.querySelector("#ingredients");
+list.append(...array);
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputNum = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDest = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+btnCreate.addEventListener("click", createBoxes);
+
+function createBoxes(e) {
+  let width = 30;
+  let height = 30;
+  for (let i = 1; i <= inputNum.value; i++) {
+    const hexColor = getRandomHexColor();
+    boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div style="background-color:${hexColor}; width: ${width}px; height:${height}px;"> </div>`
+    );
+    width += 10;
+    height += 10;
+  }
+}
+btnDest.addEventListener("click", (e) => {
+  boxes.innerHTML = "";
+});
