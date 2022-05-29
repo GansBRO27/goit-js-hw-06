05817e7d03c674988884eb1ee4aca22019fc11d0
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-05-29 22:59:49 +0300

    ред

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,10 +3,10 @@ const decrBtn = document.querySelector("[data-action='decrement']");
 const valueNum = document.querySelector("#value");
 let counterValue = 0;
 incrBtn.addEventListener("click", () => {
-  valueNum.textContent = new Number(counterValue);
   counterValue += 1;
+  valueNum.textContent = counterValue;
 });
 decrBtn.addEventListener("click", () => {
-  valueNum.textContent = new Number(counterValue);
   counterValue -= 1;
+  valueNum.textContent = counterValue;
 });
